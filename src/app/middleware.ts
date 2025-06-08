a9b206fdc1f7a3e526ec851ed26af4d0aa3f942a
ABHAY-05 <abhayvermajune5@gmail.com>
2025-06-08 17:20:38 +0530

    changes

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -1,7 +1,15 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default authMiddleware({
-  publicRoutes: ["/sign-in(.)", "/sign-up(.)", "/unauthorized"],
+const isPublicRoute = createRouteMatcher([
+  "/sign-in(.)",
+  "/sign-up(.)",
+  "/unauthorized",
+]);
+
+export default clerkMiddleware(async (auth, req) => {
+  if (!isPublicRoute(req)) {
+    await auth.protect();
+  }
 });
 
 export const config = {
